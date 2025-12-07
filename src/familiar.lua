4413f2a81278153e51f5c2550bc0c03e608078fa
Aditya Gupta <adi1998.2010@gmail.com>
2025-12-07 22:05:17 +0530

    fix swaping to base game skins

--- src/familiar.lua
+++ src/familiar.lua
@@ -5,5 +5,7 @@ modutil.mod.Path.Wrap("SetupFamiliarCostume", function (base, familiar,args)
 	if currentModCostume ~= nil then
         local costumeData = game.WorldUpgradeData[currentModCostume]
         game.SetThingProperty({ DestinationId = familiar.ObjectId, Property = "GrannyTexture", Value = costumeData.GrannyTexture })
+    else
+        game.SetThingProperty({ DestinationId = familiar.ObjectId, Property = "GrannyTexture", Value = "" })
     end
 end)

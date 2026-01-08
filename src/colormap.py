14cbfb7ac14c1597beaf5a51bfa7a2dd4772919a
Aditya Gupta <adi1998.2010@gmail.com>
2026-01-08 17:45:38 +0530

    remove hueshift reference

--- src/colormap.py
+++ src/colormap.py
@@ -5,30 +5,6 @@ import os
 import argparse
 import deppth2.texpacking as texpack
 
-def hue_shift(image_path, hue_shift=50):
-    with Image.open(image_path) as img:
-        # Convert image to RGB if it's not already
-        img = img.convert('RGBA')
-        
-        # Get image data as a list of RGB tuples
-        data = img.getdata()
-        
-        # Convert RGB to HSV, shift the hue, and convert back to RGB
-        new_data = []
-        for item in data:
-            r, g, b, a = item
-            h, s, v = colorsys.rgb_to_hsv(r/255., g/255., b/255.)
-            h = (h + hue_shift/360) % 1
-            r, g, b = colorsys.hsv_to_rgb(h, s, v)
-            new_data.append((int(r*255), int(g*255), int(b*255), a))
-        
-        # Create a new image with the shifted colors
-        new_img = Image.new(img.mode, img.size)
-        new_img.putdata(new_data)
-        
-        # new_img.show()
-        return new_img
-
 def replace_pixels(base_img, input_img):
     if input_img == None :
         return base_img

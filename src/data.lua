b1e0e700281f844729ed80da7c1330bec6ad7370
Aditya Gupta <adi1998.2010@gmail.com>
2026-04-19 22:55:58 +0530

    update darkside portraits and modifications

--- src/data.lua
+++ src/data.lua
@@ -80,7 +80,33 @@ mod.DressData = {
             Portraits_Melinoe_Casual_01 = true,
             Portraits_Melinoe_Proud_01 = true,
         },
-        DisablePortraitBlink = true,
+        PortraitOverlayModifacations = {
+            Portrait_Mel_Blink = "nil",
+            Portrait_Mel_Hesitant_Blink = "nil",
+            Portrait_Mel_Intense_Blink = "nil",
+            Portrait_Mel_Pleased_Blink = "nil",
+            Portrait_Mel_Vulnerable_Blink = "nil",
+            Portrait_Mel_LaurelGlow = {
+                Hue = 0.408,
+                VisualFx = { Name = "LaurelBurnIris", Hue = 0.408 },
+            },
+            Portrait_Mel_Hesitant_LaurelGlow = {
+                Hue = 0.408,
+                VisualFx = { Name = "LaurelBurnIris", Hue = 0.408 },
+            },
+            Portrait_Mel_Intense_LaurelGlow = {
+                Hue = 0.408,
+                VisualFx = { Name = "LaurelBurnIris", Hue = 0.408 },
+            },
+            Portrait_Mel_Pleased_LaurelGlow = {
+                Hue = 0.408,
+                VisualFx = { Name = "LaurelBurnIris", Hue = 0.408 },
+            },
+            Portrait_Mel_Vulnerable_LaurelGlow = {
+                Hue = 0.408,
+                VisualFx = { Name = "LaurelBurnIris", Hue = 0.408 },
+            },
+        },
         Color = {70, 206, 125, 255},
         LaurelCinderHue = 0.408,
     },

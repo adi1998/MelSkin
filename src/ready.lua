20385962b296b50eb98be3969c6f392733e5cb9d
Aditya Gupta <adi1998.2010@gmail.com>
2026-08-13 13:26:58 +0530

    remove duplicate func

--- src/ready.lua
+++ src/ready.lua
@@ -72,22 +72,6 @@ function mod.LoadSkinPackages()
     game.LoadPackages({Names = mod.skinPackageList})
 end
 
-function mod.dump(o, depth)
-    depth = depth or 0
-    if type(o) == 'table' then
-        local s = "\n" .. string.rep("\t", depth) .. '{\n'
-        for k,v in pairs(o) do
-            if type(k) ~= 'number' then k = '"'..k..'"' end
-            s = s .. string.rep("\t",(depth+1)) .. '['..k..'] = ' .. mod.dump(v, depth + 1) .. ',\n'
-        end
-        return s .. string.rep("\t", depth) .. '}'
-    elseif type(o) == "string" then
-        return "\"" .. o .. "\""
-    else
-        return tostring(o)
-    end
-end
-
 function mod.SetupExtraAnimations(dress)
     local laurelCindersSpawner = "LaurelCindersSpawner"
     if dress and mod.DressData[dress] and mod.DressData[dress].LaurelCinderHue then
